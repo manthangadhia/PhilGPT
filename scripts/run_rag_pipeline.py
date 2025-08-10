621beee3ac28be6b7ccdc7a18ace91da5aae237c
Manthan Gadhia <54841046+manthangadhia@users.noreply.github.com>
2025-08-10 10:54:55 +0200

    Switched out local ollama (fallback) for gemini api instead

--- scripts/run_rag_pipeline.py
+++ scripts/run_rag_pipeline.py
@@ -5,32 +5,71 @@ import pathlib
 project_root = pathlib.Path(__file__).parent.parent
 sys.path.insert(0, str(project_root))
 
-import ollama
 from utils.retriever import Retriever
 from utils.io_utils import load_system_prompt
+from google import genai
+
+from dotenv import load_dotenv
+import os
+
+# load environment variables
+load_dotenv()
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 def main(user_query, retriever=None, SYSTEM_PROMPT=None):
     if retriever is None:
         retriever = Retriever()
-
     if SYSTEM_PROMPT is None:
         SYSTEM_PROMPT = load_system_prompt()
 
-    response = ollama.chat(
-        model="gemma3:4b",
+    context = retriever.retrieve(user_query, k=8)
+
+    # first try with gemini api
+    if GEMINI_API_KEY:
+        print("Using Gemini API for response generation...")
+
+        prompt = f"""
+        {SYSTEM_PROMPT}
+
+        **CONTEXT**:
+        {context}
+
+        **USER QUERY**:
+        {user_query}
+        """
+
+        client = genai.Client(api_key=GEMINI_API_KEY)
+        response = client.models.generate_content(
+            model="gemini-2.5-flash-lite",
+            contents=prompt
+        )
+        output = response.text
+
+    else: # if no gemini api key is available, default to loading and using local ollama model
+        import ollama    
         messages=[
             {'role': "system", 'content': SYSTEM_PROMPT},
-            {'role': "assistant", 'content': Retriever().retrieve(user_query)},
+            {'role': "assistant", 'content': context},
             {'role': "user", 'content': user_query}
-        ])
+        ]
+        response = ollama.chat(
+            model="gemma3:4b",
+            messages=messages
+        )
+        output = response['message']['content']
 
-    print(f"Your question: {user_query}")
+    # print(f"Your question: {user_query}")
     print("PhilGPT's response:")
-    print(response['message']['content'])
+    print(output)
 
 if __name__ == "__main__":
     # Initialize retriever
+    import time
+    start = time.time()
     retriever = Retriever()
+    end = time.time()
+    print(f"Retriever total initialization time: {end - start} seconds")
+
     # Load system prompt
     SYSTEM_PROMPT = load_system_prompt()
 
